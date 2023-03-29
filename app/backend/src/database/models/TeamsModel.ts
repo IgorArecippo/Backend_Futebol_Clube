b1fa0c50dfa82de3779c621fc155cfc6f7c9e299
Igor Arecippo <iarecippo@gmail.com>
2023-03-29 15:08:26 -0300

    cria model e migration Teams

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,29 @@
+import { Model, INTEGER, STRING } from "sequelize";
+import db from '.';
+
+class Teams extends Model{
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    teamName: {
+      type: STRING,
+     allowNull: false,
+    },
+  },
+  
+  {
+    underscored: true,
+    timestamps: false,
+    sequelize: db,
+    modelName: 'teams',
+  },
+);
