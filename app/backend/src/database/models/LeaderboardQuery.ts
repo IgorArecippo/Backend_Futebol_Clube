7b5dee6b63d1fb3e937b439abe3e883ffc0e3fb9
Igor Arecippo <iarecippo@gmail.com>
2023-04-10 19:11:12 -0300

    finalizado o desenvolvimento do endpoint /leaderboard/home

--- app/backend/src/database/models/LeaderboardQuery.ts
+++ app/backend/src/database/models/LeaderboardQuery.ts
@@ -1,5 +1,4 @@
-const leaderboardQuery = `
-  SELECT team.team_name AS name, 
+const leaderboardQuery = `SELECT team.team_name AS name, 
     ((SUM(matches.home_team_goals > matches.away_team_goals) * 3) + 
       SUM(matches.home_team_goals = matches.away_team_goals)) AS totalPoints,
     COUNT(matches.home_team_id) AS totalGames,
@@ -7,7 +6,11 @@ const leaderboardQuery = `
     SUM(matches.home_team_goals = matches.away_team_goals) AS totalDraws,
     SUM(matches.home_team_goals < matches.away_team_goals) AS totalLosses,
     SUM(matches.home_team_goals) AS goalsFavor,
-    SUM(matches.away_team_goals) AS goalsOwn
+    SUM(matches.away_team_goals) AS goalsOwn,
+    (SUM(matches.home_team_goals) - SUM(matches.away_team_goals)) AS goalsBalance,
+FORMAT((((SUM(matches.home_team_goals > matches.away_team_goals) * 3) + 
+  SUM(matches.home_team_goals = matches.away_team_goals)) / 
+  (COUNT(matches.home_team_id) * 3) * 100), 2) AS efficiency
   FROM
     teams AS team
     JOIN matches AS matches ON team.id = matches.home_team_id
@@ -16,8 +19,8 @@ const leaderboardQuery = `
   GROUP BY
     team.team_name
   ORDER BY
-    totalPoints DESC
-    `;
-  // goalsBalance DESC,
-  // goalsFavor DESC
+    totalPoints DESC,
+    goalsBalance DESC,
+  goalsFavor DESC`;
+
 export default leaderboardQuery;
